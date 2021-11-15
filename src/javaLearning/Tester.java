6fbcdee50f5b2c6aa60678da86408888f92c741d
Laurentiu BUBUIANU <laurb95@gmail.com>
2021-11-15 17:44:08 +0200

    primele clase

---
+++ src/javaLearning/Tester.java
@@ -0,0 +1,31 @@
+package javaLearning;
+
+public class Tester {
+	
+	//--> one line comment
+	//variabile 
+	String nume;
+	int ratePerHour;
+	
+	//metode
+	
+	public String getNume() {
+		return nume;
+	}
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+	public int getRatePerHour() {
+		return ratePerHour;
+	}
+	public void setRatePerHour(int ratePerHour) {
+		this.ratePerHour = ratePerHour;
+	}
+		
+	
+	
+
+
+
+
+}
---
+++ src/javaLearning/WageCalculator.java
@@ -0,0 +1,32 @@
+package javaLearning;
+
+public class WageCalculator {
+	
+	int nrOreLucrate = 40;
+
+	public static void main(String[] args) {
+		Tester tester1 = new Tester();
+		tester1.setNume("Oana");
+		tester1.setRatePerHour(40);
+		
+		System.out.println(tester1.getNume());
+		
+		Tester tester2 = new Tester();
+		tester2.setNume("Maria");
+		tester2.setRatePerHour(25);
+
+		System.out.println(tester2.getNume());
+		
+		WageCalculator calc = new WageCalculator();
+		
+		System.out.println("Salariul lui " + tester1.getNume() + " este " + calc.calculateSalary(tester1.getRatePerHour()));
+		
+	}
+
+	public int calculateSalary(int rateHourly) {
+		int salary = nrOreLucrate * rateHourly;		
+		return salary;
+	}
+	
+	
+}
